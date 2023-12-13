f06950c8c74749ba3be5acd217f319d753698208
FelipeTDR31 <felipetdrgames@gmail.com>
2023-12-13 19:56:04 -0300

    banco de dados

--- backend/tinderproject.sql
+++ backend/tinderproject.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/12/2023 às 03:29
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 13/12/2023 às 23:55
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,23 +39,21 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `title`, `image`) VALUES
 (8, 'Javascript', 'uploads/Javascript_popeg.png'),
-(9, 'Typescript', 'uploads/Typescript_vc4q0.png'),
-(10, 'JuliaLang', 'uploads/JuliaLang_7ns8l.png'),
-(12, 'RLang', 'uploads/RLang_avw2j.png'),
-(13, 'Java', 'uploads/Java_m16rr.png'),
-(14, 'CSS', 'uploads/CSS_di00j.png'),
-(15, 'HTML5', 'uploads/HTML5_2pnr5.png'),
-(16, 'Python', 'uploads/Python_mwg1h.png'),
-(17, 'PHP', 'uploads/PHP_d6u19.png'),
-(18, 'Swift', 'uploads/Swift_io0re.png'),
-(19, 'Kotlin', 'uploads/Kotlin_j0mwf.jpeg'),
-(20, 'C++', 'uploads/C++_g4d68.png'),
-(21, 'Csharp', 'uploads/Csharp_0e08v.png'),
-(22, 'Assembly', 'uploads/Assembly_pkjnq.png'),
-(100, 'GoLang', 'uploads/GoLang_547bn.png'),
-(101, 'Scala', 'uploads/Scala_yco9c.png'),
-(117, 'LuaLang', 'uploads/LuaLang_f1yeg.png'),
-(118, 'React', 'uploads/React_8l2cw.png');
+(119, 'Typescript', 'uploads/Typescript_thir9.png'),
+(120, 'LuaLang', 'uploads/LuaLang_wk424.png'),
+(121, 'Assembly', 'uploads/Assembly_teep3.png'),
+(122, 'Python', 'uploads/Python_j57g6.jpeg'),
+(123, 'Java', 'uploads/Java_sjryf.png'),
+(124, 'JuliaLang', 'uploads/JuliaLang_stpjy.png'),
+(125, 'Kotlin', 'uploads/Kotlin_2izk7.jpeg'),
+(126, 'C++', 'uploads/C++_cgqjf.png'),
+(127, 'Csharp', 'uploads/Csharp_i04ty.png'),
+(128, 'Cobol', 'uploads/Cobol_0i999.png'),
+(129, 'GoLang', 'uploads/GoLang_3t18t.png'),
+(130, 'React', 'uploads/React_ygs3o.png'),
+(131, 'CSS', 'uploads/CSS_tl9dr.jpeg'),
+(132, 'HTML', 'uploads/HTML_ngxtd.png'),
+(133, 'Vuejs', 'uploads/Vuejs_qjj71.jpg');
 
 -- --------------------------------------------------------
 
@@ -77,7 +75,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `email`, `username`, `password`, `is_Admin`) VALUES
 (1, 'admin123@gmail.com', 'admin', 'senha123', 1),
-(7, 'felipe.gomes@aluno.feliz.ifrs.edu.br', 'Felipe', 'afr21', 0);
+(7, 'felipe.gomes@aluno.feliz.ifrs.edu.br', 'Felipe', 'afr21', 0),
+(13, 'charles@aluno.feliz.ifrs.edu.br', 'Charles', '123456', 0),
+(14, 'igor@aluno.feliz.ifrs.edu.br', 'Igor', '654321', 0),
+(15, 'lucas@aluno.feliz.ifrs.edu.br', 'Lucas', '123', 0);
 
 -- --------------------------------------------------------
 
@@ -91,6 +92,75 @@ CREATE TABLE `user_item` (
   `likeQuantity` int(11) NOT NULL,
   `dislikeQuantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `user_item`
+--
+
+INSERT INTO `user_item` (`userID`, `itemID`, `likeQuantity`, `dislikeQuantity`) VALUES
+(7, 8, 1, 0),
+(7, 119, 1, 0),
+(7, 120, 0, 1),
+(7, 121, 1, 0),
+(7, 122, 0, 1),
+(7, 123, 1, 0),
+(7, 124, 0, 1),
+(7, 125, 1, 0),
+(7, 126, 1, 0),
+(7, 127, 1, 0),
+(7, 128, 1, 0),
+(7, 129, 0, 1),
+(7, 130, 1, 0),
+(7, 131, 0, 1),
+(7, 132, 1, 0),
+(7, 133, 1, 0),
+(13, 8, 1, 0),
+(13, 119, 1, 0),
+(13, 120, 0, 1),
+(13, 121, 1, 0),
+(13, 122, 1, 0),
+(13, 123, 0, 1),
+(13, 124, 1, 0),
+(13, 125, 0, 1),
+(13, 126, 1, 0),
+(13, 127, 1, 0),
+(13, 128, 1, 0),
+(13, 129, 0, 1),
+(13, 130, 1, 0),
+(13, 131, 1, 0),
+(13, 132, 1, 0),
+(14, 8, 1, 0),
+(14, 119, 1, 0),
+(14, 120, 0, 1),
+(14, 121, 1, 0),
+(14, 122, 1, 0),
+(14, 123, 0, 1),
+(14, 124, 1, 0),
+(14, 125, 0, 1),
+(14, 126, 1, 0),
+(14, 127, 1, 0),
+(14, 128, 0, 1),
+(14, 129, 1, 0),
+(14, 130, 1, 0),
+(14, 131, 0, 1),
+(14, 132, 0, 1),
+(14, 133, 1, 0),
+(15, 8, 1, 0),
+(15, 119, 1, 0),
+(15, 120, 0, 1),
+(15, 121, 1, 0),
+(15, 122, 1, 0),
+(15, 123, 1, 0),
+(15, 124, 0, 1),
+(15, 125, 0, 1),
+(15, 126, 0, 1),
+(15, 127, 0, 1),
+(15, 128, 0, 1),
+(15, 129, 0, 1),
+(15, 130, 0, 1),
+(15, 131, 0, 1),
+(15, 132, 0, 1),
+(15, 133, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -156,13 +226,13 @@ ALTER TABLE `_prisma_migrations`
 -- AUTO_INCREMENT de tabela `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
 
 --
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restrições para tabelas despejadas
